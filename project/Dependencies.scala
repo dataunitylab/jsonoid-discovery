7815de2047324d871c8e350eeee135c4999e515e
Michael Mior <mmior@cs.rit.edu>
2021-04-03 20:42:55 -0400

    Update dependency versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 import sbt._
 
 object Dependencies {
-  lazy val json4s              = "org.json4s"    %% "json4s-jackson"         % "3.7.0-M11"
-  lazy val json4sScalaz        = "org.json4s"    %% "json4s-scalaz"          % "3.7.0-M11"
+  lazy val json4s              = "org.json4s"    %% "json4s-jackson"         % "3.7.0-M15"
+  lazy val json4sScalaz        = "org.json4s"    %% "json4s-scalaz"          % "3.7.0-M15"
   lazy val jsonSchemaValidator = "com.networknt" %  "json-schema-validator"  % "1.0.51"
 
-  lazy val scalaTest           = "org.scalatest" %% "scalatest"              % "3.2.2"
+  lazy val scalaTest           = "org.scalatest" %% "scalatest"              % "3.2.7"
 }
