e26462b7484a0300ecfd8d923a678fbff9ad7733
Michael Mior <mmior@cs.rit.edu>
2021-04-07 14:35:21 -0400

    Add tests for merging NumberSchema with IntegerSchema

--- src/test/scala/edu/rit/cs/mmior/jsonoid/discovery/schemas/NumberSchemaSpec.scala
+++ src/test/scala/edu/rit/cs/mmior/jsonoid/discovery/schemas/NumberSchemaSpec.scala
@@ -6,18 +6,33 @@ import UnitSpec._
 class NumberSchemaSpec extends UnitSpec {
   behavior of "NumberSchema"
 
-  private val numberSchema = NumberSchema(3.14).properties.merge(4.28)
+  private val numberSchema = NumberSchema(3.14).merge(NumberSchema(4.28))
+  private val integerSchema = IntegerSchema(5)
+  private val mixedSchema = numberSchema.merge(integerSchema)
 
-  it should "track the maximum length" in {
-    numberSchema should contain (MaxNumValueProperty(Some(4.28)))
+  it should "track the maximum value" in {
+    numberSchema.properties should contain (MaxNumValueProperty(Some(4.28)))
   }
 
-  it should "track the minimum length" in {
-    numberSchema should contain (MinNumValueProperty(Some(3.14)))
+  it should "track the minimum value" in {
+    numberSchema.properties should contain (MinNumValueProperty(Some(3.14)))
   }
 
   it should "track the distinct elements" in {
-    val hyperLogLogProp = numberSchema.find(_.isInstanceOf[NumHyperLogLogProperty]).fold(NumHyperLogLogProperty())(_.asInstanceOf[NumHyperLogLogProperty])
+    val hyperLogLogProp = numberSchema.properties.find(_.isInstanceOf[NumHyperLogLogProperty]).fold(NumHyperLogLogProperty())(_.asInstanceOf[NumHyperLogLogProperty])
     hyperLogLogProp.hll.count() should be (2)
+  }
+
+  it should "track the maximum value when merged with an integer schema" in {
+    mixedSchema.properties should contain (MaxNumValueProperty(Some(5)))
+  }
+
+  it should "track the minimum value when merged with an integer schema" in {
+    mixedSchema.properties should contain (MinNumValueProperty(Some(3.14)))
+  }
+
+  it should "track the distinct elements when merged with an integer schema" in {
+    val hyperLogLogProp = mixedSchema.properties.find(_.isInstanceOf[NumHyperLogLogProperty]).fold(NumHyperLogLogProperty())(_.asInstanceOf[NumHyperLogLogProperty])
+    hyperLogLogProp.hll.count() should be (3)
   }
 }
