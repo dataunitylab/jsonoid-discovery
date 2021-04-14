d72554154f4162e6fcea38e58b081171e7102657
Michael Mior <mmior@cs.rit.edu>
2021-04-14 12:41:41 -0400

    Allow finding foreign keys on resulting schemas

---
+++ src/main/scala/edu/rit/cs/mmior/jsonoid/discovery/ForeignKeyFinder.scala
@@ -0,0 +1,67 @@
+package edu.rit.cs.mmior.jsonoid.discovery
+
+import scala.reflect.classTag
+
+import com.sangupta.bloomfilter.impl.InMemoryBloomFilter
+
+import schemas._
+
+final case class ForeignKey(localPath: String, foreignPath: String)
+
+object ForeignKeyFinder {
+  @SuppressWarnings(Array("org.wartremover.warts.Recursion"))
+  private def collectFiltersWithPrefix(
+      prefix: String,
+      schema: JsonSchema[_]
+  ): Seq[(String, InMemoryBloomFilter[_])] = {
+    schema match {
+      case o: ObjectSchema =>
+        val props = o.properties.get[ObjectTypesProperty].objectTypes
+        props.keySet.toSeq.flatMap(key =>
+          collectFiltersWithPrefix(prefix + "." + key, props(key))
+        )
+      case a: ArraySchema =>
+        collectFiltersWithPrefix(
+          prefix + "[*]",
+          a.properties.get[ItemTypeProperty].itemType
+        )
+      case i: IntegerSchema =>
+        Seq((prefix, i.properties.get[IntBloomFilterProperty].bloomFilter))
+      case n: NumberSchema =>
+        Seq((prefix, n.properties.get[NumBloomFilterProperty].bloomFilter))
+      case s: StringSchema =>
+        Seq((prefix, s.properties.get[StringBloomFilterProperty].bloomFilter))
+      case _ => Seq.empty[(String, InMemoryBloomFilter[_])]
+    }
+  }
+
+  def collectFiltersByPath(
+      schema: JsonSchema[_]
+  ): Map[String, InMemoryBloomFilter[_]] = {
+    collectFiltersWithPrefix("$", schema).toMap
+  }
+
+  def findForeignKeys(schema: JsonSchema[_]): List[ForeignKey] = {
+    val filters = collectFiltersByPath(schema)
+
+    filters.keySet.toSeq
+      .combinations(2)
+      .flatMap { case Seq(pathA, pathB) =>
+        val filterA = filters(pathA)
+        val filterB = filters(pathB)
+
+        @SuppressWarnings(Array("org.wartremover.warts.Var"))
+        var keys = Seq.empty[ForeignKey]
+
+        if (filterA.maybeSubsetOf(filterB)) {
+          keys = keys ++ Seq(ForeignKey(pathA, pathB))
+        }
+        if (filterB.maybeSubsetOf(filterA)) {
+          keys = keys ++ Seq(ForeignKey(pathB, pathA))
+        }
+
+        keys
+      }
+      .toList
+  }
+}
---
+++ src/test/scala/edu/rit/cs/mmior/jsonoid/discovery/ForeignKeyFinderSpec.scala
@@ -0,0 +1,16 @@
+package edu.rit.cs.mmior.jsonoid.discovery
+
+import org.json4s.JsonDSL._
+import org.json4s._
+
+class ForeignKeyFinderSpec extends UnitSpec {
+  behavior of "ForeignKeyFinder"
+
+  it should "find possible foreign keys" in {
+    val jsons: Seq[JValue] = Seq(("a" -> 3) ~ ("b" -> 4), ("a" -> 3) ~ ("b" -> 3))
+    val schema = DiscoverSchema.discover(jsons)
+    val foreignKeys = ForeignKeyFinder.findForeignKeys(schema)
+
+    foreignKeys shouldEqual List(ForeignKey("$.a", "$.b"))
+  }
+}
