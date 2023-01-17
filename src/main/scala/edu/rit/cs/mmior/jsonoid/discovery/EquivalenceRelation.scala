ac6f1207783a07c9d37eeef730f66a817d656780
Michael Mior <mmior@mail.rit.edu>
2023-01-17 09:29:43 -0500

    Add equivalence relation which checks types

--- src/main/scala/edu/rit/cs/mmior/jsonoid/discovery/EquivalenceRelation.scala
+++ src/main/scala/edu/rit/cs/mmior/jsonoid/discovery/EquivalenceRelation.scala
@@ -35,6 +35,24 @@ object EquivalenceRelations {
   }
 
   @SuppressWarnings(Array("org.wartremover.warts.Equals"))
+  implicit object TypeMatchEquivalenceRelation extends EquivalenceRelation {
+    def fuse(kind1: JsonSchema[_], kind2: JsonSchema[_]): Boolean = {
+      (kind1, kind2) match {
+        case (obj1: ObjectSchema, obj2: ObjectSchema) => {
+          val types1 = obj1.properties.get[ObjectTypesProperty].objectTypes
+          val types2 = obj2.properties.get[ObjectTypesProperty].objectTypes
+
+          val intersectingKeys = types1.keySet & types2.keySet
+          intersectingKeys.forall { key =>
+            types1(key).schemaType == types2(key).schemaType
+          }
+        }
+        case _ => kind1.schemaType === kind1.schemaType
+      }
+    }
+  }
+
+  @SuppressWarnings(Array("org.wartremover.warts.Equals"))
   implicit object KindEquivalenceRelation extends EquivalenceRelation {
     def fuse(kind1: JsonSchema[_], kind2: JsonSchema[_]): Boolean = {
       kind1.schemaType === kind1.schemaType
--- src/test/scala/edu/rit/cs/mmior/jsonoid/discovery/EquivalenceRelationSpec.scala
+++ src/test/scala/edu/rit/cs/mmior/jsonoid/discovery/EquivalenceRelationSpec.scala
@@ -3,7 +3,6 @@ package edu.rit.cs.mmior.jsonoid.discovery
 import schemas._
 
 class EquivalenceRelationSpec extends UnitSpec {
-  behavior of "EquivalenceRelation"
 
   implicit val propSet: PropertySet = PropertySets.MinProperties
 
@@ -12,6 +11,11 @@ class EquivalenceRelationSpec extends UnitSpec {
   val objSchema3: ObjectSchema = ObjectSchema(
     Map("foo" -> BooleanSchema(), "bar" -> BooleanSchema())
   )
+  val objSchema4: ObjectSchema = ObjectSchema(
+    Map("foo" -> IntegerSchema(), "bar" -> BooleanSchema())
+  )
+
+  behavior of "KindEquivalenceRelation"
 
   it should "keep schemas separate when merging by label" in {
     val merged = objSchema1.merge(objSchema2)(
@@ -30,17 +34,23 @@ class EquivalenceRelationSpec extends UnitSpec {
     )
   }
 
+  behavior of "LabelEquivalenceRelation"
+
   it should "create ProductSchemas when merging non-objects" in {
     val merged = objSchema1
       .merge(IntegerSchema(0))(EquivalenceRelations.LabelEquivalenceRelation)
     merged shouldBe a[ProductSchema]
   }
 
+  behavior of "NonEquivalenceRelation"
+
   it should "not merge when using non-equivalence" in {
     val merged =
       objSchema1.merge(objSchema1)(EquivalenceRelations.NonEquivalenceRelation)
     merged shouldBe a[ProductSchema]
   }
+
+  behavior of "IntersectingLabelEquivalenceRelation"
 
   it should "separate schemas which have no common labels" in {
     val merged = objSchema1.merge(objSchema2)(
@@ -59,5 +69,26 @@ class EquivalenceRelationSpec extends UnitSpec {
       "foo" -> BooleanSchema(),
       "bar" -> BooleanSchema()
     )
+  }
+
+  behavior of "TypeMatchEquivalenceRelation"
+
+  it should "combine schemas with the same type" in {
+    val merged = objSchema1
+      .merge(objSchema3)(
+        EquivalenceRelations.TypeMatchEquivalenceRelation
+      )
+      .asInstanceOf[ObjectSchema]
+    merged.properties.get[ObjectTypesProperty].objectTypes shouldBe Map(
+      "foo" -> BooleanSchema(),
+      "bar" -> BooleanSchema()
+    )
+  }
+
+  it should "separate schemas which different types" in {
+    val merged = objSchema1.merge(objSchema4)(
+      EquivalenceRelations.TypeMatchEquivalenceRelation
+    )
+    merged shouldBe a[ProductSchema]
   }
 }
