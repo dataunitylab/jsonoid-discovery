c269a6e70d8fa9b5b6ee29651e6ab772f46039ec
Michael Mior <mmior@mail.rit.edu>
2023-05-19 13:18:47 -0400

    Run fuzzer in temp directory

--- run-fuzzer.sh
+++ run-fuzzer.sh
@@ -3,12 +3,15 @@
 # Get the assembly jar file name
 JAR_FILE=$(sbt 'fuzz / assembly; fuzz / assembly / assemblyJarName' 2> /dev/null | grep -Eo '/.*\.jar$')
 
-# Copy JSON test files to the fuzzer directory
-cp -r fuzz/test/resources fuzz/target/scala-2.13
+# Copy test files to the temporary test directory
+TMP_DIR=$(mktemp -d)
+cp fuzz/test/resources/json.dict $TMP_DIR
+cp -r src/test/resources  $TMP_DIR
+cp $JAR_FILE $TMP_DIR
 
 set -o xtrace
-docker run -v $(pwd)/fuzz/target/scala-2.13:/fuzzing cifuzz/jazzer \
+docker run -v $TMP_DIR:/fuzzing cifuzz/jazzer \
            --cp=$(basename $JAR_FILE) \
            --target_class=edu.rit.cs.dataunitylab.jsonoid.discovery.DiscoverSchemaFuzzer \
-           -dict=resources/json.dict \
+           -dict=json.dict \
            resources
