50f80d4161c95c7cf71c082f33a09acb0e9f618a
Michael Mior <mmior@mail.rit.edu>
2023-05-20 19:57:29 -0400

    Remove unnecessary warnings in fuzzer

--- fuzz/src/main/scala/edu/rit/cs/dataunitylab/jsonoid/discovery/DiscoverSchemaFuzzer.scala
+++ fuzz/src/main/scala/edu/rit/cs/dataunitylab/jsonoid/discovery/DiscoverSchemaFuzzer.scala
@@ -4,12 +4,6 @@ import scala.io.Source
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider
 
-@SuppressWarnings(
-  Array(
-    "org.wartremover.warts.NonUnitStatements",
-    "org.wartremover.warts.Return"
-  )
-)
 object DiscoverSchemaFuzzer {
   def fuzzerTestOneInput(data: FuzzedDataProvider): Unit = {
     val input = data.consumeRemainingAsString()
