614118702593b4c443a15c302007cee29c679e7f
Michael Mior <mmior@mail.rit.edu>
2023-05-20 20:01:16 -0400

    Add fuzzer for JSON Schema deserialization

---
+++ fuzz/src/main/scala/edu/rit/cs/dataunitylab/jsonoid/discovery/schemas/JsonSchemaFuzzer.scala
@@ -0,0 +1,20 @@
+package edu.rit.cs.dataunitylab.jsonoid.discovery
+package schemas
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider
+import org.json4s.jackson.JsonMethods._
+
+object JsonSchemaFuzzer {
+  def fuzzerTestOneInput(data: FuzzedDataProvider): Unit = {
+    val input = data.consumeRemainingAsString()
+    try {
+      val json = parse(input)
+      JsonSchema.fromJson(json)
+    } catch {
+      case e: com.fasterxml.jackson.core.JsonParseException =>
+      case e: com.fasterxml.jackson.databind.exc.MismatchedInputException =>
+      case e: UnsupportedOperationException =>
+    }
+  }
+}
+
