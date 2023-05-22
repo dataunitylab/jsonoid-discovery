5568f1df6762ecaa278a089c9bb83d8102d6263a
Michael Mior <mmior@mail.rit.edu>
2023-05-22 10:39:10 -0400

    Add tests for PropertySet

---
+++ src/test/scala/edu/rit/cs/dataunitylab/jsonoid/discovery/schemas/PropertySetsSpec.scala
@@ -0,0 +1,52 @@
+package edu.rit.cs.dataunitylab.jsonoid.discovery
+package schemas
+
+class PropertySetsSpec extends UnitSpec {
+  behavior of "SchemaProperties"
+
+  it should "be able to include only properties which match a given name" in {
+    val propSet =
+      PropertySets.AllProperties.onlyNamed(List("MinLengthProperty"))
+    propSet.arrayProperties shouldBe empty
+    propSet.booleanProperties shouldBe empty
+    propSet.integerProperties shouldBe empty
+    propSet.numberProperties shouldBe empty
+    propSet.objectProperties shouldBe empty
+    propSet.stringProperties.properties.keySet
+      .map(_.toString) should contain theSameElementsAs List(
+      "edu.rit.cs.dataunitylab.jsonoid.discovery.schemas.MinLengthProperty"
+    )
+  }
+
+  it should "be able to include only properties which match a given class" in {
+    val propSet =
+      PropertySets.AllProperties.only(List(classOf[MinLengthProperty]))
+    propSet.arrayProperties shouldBe empty
+    propSet.booleanProperties shouldBe empty
+    propSet.integerProperties shouldBe empty
+    propSet.numberProperties shouldBe empty
+    propSet.objectProperties shouldBe empty
+    propSet.stringProperties.properties.keySet
+      .map(_.toString) should contain theSameElementsAs List(
+      "edu.rit.cs.dataunitylab.jsonoid.discovery.schemas.MinLengthProperty"
+    )
+  }
+
+  it should "be able to exclude only properties which don't match a given name" in {
+    val propSet =
+      PropertySets.AllProperties.withoutNamed(List("MinLengthProperty"))
+    propSet.stringProperties.properties.keySet
+      .map(
+        _.toString
+      ) should not contain ("edu.rit.cs.dataunitylab.jsonoid.discovery.schemas.MinLengthProperty")
+  }
+
+  it should "be able to exclude only properties which don't match a given class" in {
+    val propSet =
+      PropertySets.AllProperties.without(List(classOf[MinLengthProperty]))
+    propSet.stringProperties.properties.keySet
+      .map(
+        _.toString
+      ) should not contain ("edu.rit.cs.dataunitylab.jsonoid.discovery.schemas.MinLengthProperty")
+  }
+}
