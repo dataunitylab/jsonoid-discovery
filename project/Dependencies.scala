7c84e99511e007b708ff4c3e433b5a30c35a8853
Michael Mior <mmior@mail.rit.edu>
2024-07-23 16:50:56 -0400

    Update scala-csv to 2.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
   lazy val jsonSchemaValidator = "com.networknt"               % "json-schema-validator" % "1.5.0"
   lazy val openLocationCode    = "com.google.openlocationcode" % "openlocationcode"      % "1.0.4"
   lazy val scopt               = "com.github.scopt"           %% "scopt"                 % "4.1.0"
-  lazy val scalaCsv            = "com.github.tototoshi"       %% "scala-csv"             % "1.4.1"
+  lazy val scalaCsv            = "com.github.tototoshi"       %% "scala-csv"             % "2.0.0"
   lazy val spark               = "org.apache.spark"           %% "spark-core"            % "3.5.1"
   lazy val sparkSql            = "org.apache.spark"           %% "spark-sql"             % "3.5.1"
   lazy val validator           = "commons-validator"           % "commons-validator"     % "1.9.0"
